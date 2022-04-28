53a5d4b6b1aad94627cdb6f274a27faddec579e5
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-04-28 13:39:01 +0200

    update

--- Projects/mocha-intro/yearsArgo.tests.js
+++ Projects/mocha-intro/yearsArgo.tests.js
@@ -1,8 +1,8 @@
 describe ("the yearsAgo function", function(){
-    it("should return a number years between the current year and 1976 with '46'", function(){
+    it("should return the number years between the current year and 1976 with '46'", function(){
         assert.equal(46, yearsAgo(1976));
     })
-    it("should return a  number years between the current year and 1976 with '22'", function(){
+    it("should return the number years between the current year and 1976 with '22'", function(){
         assert.equal(22, yearsAgo(2000));
     })
 })
